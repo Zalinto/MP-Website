33b559336dd471cb7346d34d753e02a2aebecdee
Zalinto <jndechave@gmail.com>
2024-04-23 19:32:08 -0700

    Add project files.

---
+++ MP-Microtia-Website/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MP_Microtia_Website.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
